33192fff7cac79da5b4029fc60ee2f441e2b0b93
Chris Nelson <chrisnelson7791@gmail.com>
2024-06-04 12:13:22 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function reverseList(list) {
+
+  return list.reverse()
+}
+
+console.log(reverseList([1, 3, 5, 7, 9]))
+
+// params: list   // an array
+// return list    // reversed array
+// e.g. reverseList([1, 3, 5, 7, 9])
+// results in [9, 7, 5, 3, 1]
+
+
+// use reverse method on list and return the result
